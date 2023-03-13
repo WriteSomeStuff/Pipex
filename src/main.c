a3663e9b6b2edca6dca7c154dc03d432f310a05c
Cheyenne <109852625+WriteSomeStuff@users.noreply.github.com>
2023-03-13 18:09:51 +0100

    practice code

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cschabra <cschabra@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/09 14:09:19 by cschabra      #+#    #+#                 */
-/*   Updated: 2023/03/13 13:57:30 by cschabra      ########   odam.nl         */
+/*   Updated: 2023/03/13 18:05:04 by cschabra      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ int32_t	main(void)
 {
 	int	fd[2];
 	int	id;
+	int	x;
+	int	y;
 
+	y = 0;
 	if (pipe(fd) == -1)
 	{
 		ft_putendl_fd("pipe error", STDERR_FILENO);
@@ -32,7 +35,26 @@ int32_t	main(void)
 	if (id == 0)
 	{
 		close(fd[0]);
-		//do something
+		printf("input a number: ");
+		scanf("%d", &x);
+		if (write(fd[1], &x, sizeof(int)) == -1)
+		{
+			ft_putendl_fd("write error", STDERR_FILENO);
+			return (3);
+		}
+		close(fd[1]);
+	}
+	else
+	{
+		close(fd[1]);
+		if (read(fd[0], &y, sizeof(int)) == -1)
+		{
+			ft_putendl_fd("read error", STDERR_FILENO);
+			return (4);
+		}
+		y = y * 2;
+		close(fd[0]);
+		printf("got from child process: %d\n", y);
 	}
 	return (0);
 }
